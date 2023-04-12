83bc0a9270c1b149edb7d2ef6bd45dc0f608ecd3
jayrider1218 <leungkitlun@gmail.com>
2023-04-12 22:45:49 +0800

    Updated leaderBoard

--- Source/Assets/Scripts/LeaderBoardManager.cs
+++ Source/Assets/Scripts/LeaderBoardManager.cs
@@ -1,31 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using Firebase.Database;
+using Firebase.Extensions;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class LeaderBoardManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     [SerializeField]
     public DataBaseManager firebasemanager;
     public Board leaderBoard;
+    [SerializeField]
+    public const int BOARDLENGTH = 5;
+    [SerializeField]
+    public TMP_Text outputScore;
+    [SerializeField]
+    public TMP_Text outputName;
+    [SerializeField]
+    public TMP_Text outputRank;
     public class Entries {
         public string UserName;
         public int score;
         public Entries() {
             UserName = "User";
             score = 0;
+            //Debug.Log("Entries");
         }
     }
     public class Board {
         public Entries[] entries;
         public Board() {
-            entries = new Entries[5];
+            entries = new Entries[BOARDLENGTH];
+            for (int i = 0; i < entries.Length; i++) {
+                entries[i] = new Entries();
+            }
+
         }
-        public void printBoard() {
-            foreach (var entry in entries) {
-                print(entry.UserName+ " " + entry.score);
+        public void printEntires() {
+
+            foreach (Entries entry in entries) {
+                print(entry.UserName + " " + entry.score);
             }
         }
     }
@@ -33,25 +50,39 @@ public class LeaderBoardManager : MonoBehaviour
     void Start() {
         leaderBoard = new Board();
     }
- 
+
     public void PrintBoard() {
-        firebasemanager.GetReference().Child("LeaderBoard").OrderByChild("score").LimitToFirst(5).GetValueAsync().ContinueWith(task => {
+        outputRank.text = "Rank\n1\n2\n3\n4\n5\nYou";
+        outputName.text = "Name\n";
+        outputScore.text = "Score\n";
+        for (int i = 0; i < leaderBoard.entries.Length ; i++) {
+            outputName.text += leaderBoard.entries[i].UserName + "\n";
+            outputScore.text += leaderBoard.entries[i].score.ToString() + "\n";
+        }
+        outputName.text += firebasemanager.profile.UserName;
+        outputScore.text += firebasemanager.profile.HighestScore.ToString();
+    }
+
+    public void StoreBoard() {
+        firebasemanager.GetReference().Child("LeaderBoard").OrderByChild("score").LimitToFirst(BOARDLENGTH).GetValueAsync().ContinueWithOnMainThread(task => {
             if (task.IsCompletedSuccessfully) {
                     DataSnapshot val = task.Result;
                     print(val.ChildrenCount.ToString());
                     int i = 4;
                     Debug.Log("start reading entry.");
                     foreach (var entry in val.Children) {
-                        Debug.Log("entry: " + i.ToString());
+                        //Debug.Log("entry: " + i.ToString());
                         var json = entry.GetRawJsonValue();
-                        Debug.Log("Transfered to json");
+                        //Debug.Log("Transfered to json");
                         leaderBoard.entries[i] = JsonUtility.FromJson<Entries>(json);
-                        Debug.Log("added to borad.");
+                        //Debug.Log("added to borad.");
                         i--;
                     }
-                    leaderBoard.printBoard();
+                    PrintBoard();
                 } else print("Print Failed.");
         });
+
+        leaderBoard.printEntires();
     }
     public void UpdateScore(int score) {
         if (score > firebasemanager.profile.HighestScore) {
@@ -73,7 +104,7 @@ public class LeaderBoardManager : MonoBehaviour
 
         if (leaders == null) {
             leaders = new List<object>();
-        } else if (mutableData.ChildrenCount >= 5) {
+        } else if (mutableData.ChildrenCount >= BOARDLENGTH) {
             long minScore = long.MaxValue;
             object minVal = null;
             foreach (var child in leaders) {
