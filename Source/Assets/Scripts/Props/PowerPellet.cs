e28b31bb734d3d9612ba9deee7de8a71689a2250
Mokmokhi <owenmok202@outlook.com>
2023-04-13 00:17:13 +0800

    power pellet and props manager

--- Source/Assets/Scripts/Props/PowerPellet.cs
+++ Source/Assets/Scripts/Props/PowerPellet.cs
@@ -1,11 +1,18 @@
+using System.Net.NetworkInformation;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class PowerPellet : Pellet {
-    public float duration = 8f;
+    public int level = 1;
+    public float duration = 8f+level*1f;
+
+    // level would be changed by shop function
+    // duration would be updated when level is changed
 
     protected override void Eaten() {
+        GameObject.FindWithTag("Pacman").GetComponent<Movement>().speedMultiplier=1f+level*0.1f;
         GameObject.FindWithTag("Pacman").GetComponent<Pacman>().EatPowerPellet(this);
+        GameObject.FindWithTag("Pacman").GetComponent<Movement>().speedMultiplier=1f;
         gameObject.SetActive(false);
     }
-
 }
